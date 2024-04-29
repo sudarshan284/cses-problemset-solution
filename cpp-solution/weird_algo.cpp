39a63ad0059335b548630ff76db56c135a236079
sudarshan284 <sudarshanthakur18001@gmail.com>
2024-04-29 23:55:09 +0530

    comment added in the code

--- cpp-solution/weird_algo.cpp
+++ cpp-solution/weird_algo.cpp
@@ -8,13 +8,14 @@ int main()
     cout << n << " ";
     while (n > 1)
     {
-        if (n & 1)
+        if (n & 1) // is a bitwise AND operation. Since in binary representation, odd numbers have the least significant bit set to 1,
+                   // this operation checks if the least significant bit of n is 1, indicating n is odd
         {
             n = (3 * n) + 1;
         }
         else
         {
-            n >>= 1;
+            n >>= 1; // is right shift operation which is equivalent to divide by 2;
         }
         cout << n << " ";
     }
